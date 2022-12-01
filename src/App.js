39133c08d4bc288b3f3dc6148db615e80a4efa16
victorialange <viclan2009@hotmail.com>
2022-11-30 20:40:57 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
+
+
 function App() {
+
+  // use effect
+  useEffect( () => {
+    axios({
+      url: "https://the-trivia-api.com/api/questions",
+      method: "GET",
+      dataResponse: "json",
+      params: {
+        limit: 1
+      },
+    })
+    .then( (response) => {
+      console.log(response.data);
+      console.log(response.data[0].difficulty);
+
+  });
+}, []); 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Trivia!</h1>
     </div>
   );
 }
 
 export default App;
+
+
+// PSEUDO CODE
+
+// Get data (questions, category, incorrectAnswers, correctAnswer) from the Trivia API when App.js mounts
+// Use (import) Axios to make the API call
+// Only make the API call once when the App.js mounts, so for that use useEffect with an empty dependency array (import useEffect).
+
+// Have a button on the page that lets user start the trivia question. On pressing the button (click), start displaying question and answer possibilities (label and checkbox, can only check one). Also have a return to start or get a different question button, in case the user doesn't like this question (for this also an event listener onClick that lets user return to start display, when clicked)
+
+// Add an event listener function clickHandler to only display the data from the API, when the user has clicked the button.
+
+// Use map to return new array with the data and render it onto the page. For that use useState (import useState), which will trigger a re-render to the page and allows me to return new JSX with the questions and choices on the page.
+
+// Display the questions and answers from the API in its own component(displayQuestions) and import the component into App.js. Use props inside the component to pass down functions from App.js. In there write JSX to display the questions and answer choices, plus a sumbit button and another restart button. Also include an if statement, that returns either a "GOOD JOB!" or a "BETTER LUCK NEXT TIME!" statement based on whether the user chose the right or wrong anwer (userChoice == correctAnswer, or userChoice !== incorrectAnswers) and a button that lets user start a new game with a different question (anotherOnClick) (all inside another event listener function upon form submission - submitHandler).
+
+// inside the onSubmitHandler make submit button from quiz and the restart disappear, and based on the if statement (which conditions apply), display either right or wrong statement and the option to start a new game (newGameClickHandler).
+
+// whole quiz == form
+// question == label
+// possible answers == inputs with type=checkbox
+// buttons (return and submit answer)
+// submit answer button with type=submit
